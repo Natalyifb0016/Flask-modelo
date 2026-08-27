066aa72b50008fccd9d2e61bac1836b34fc1e64c
Natalyifb0016 <natalydamaceno01@gmail.com>
2026-08-27 10:43:58 -0300

    novo esquema

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -9,6 +9,16 @@ def login():
 @app.route("/painel")
 def painel():
     return render_template("painel.html") 
+
+@app.route("/verificar", methods=['POST'])
+def verificar():
+
+    cpf = request.form.get('cpf')
+    senha = request.form.get('senha')
+
+    print('tentando login com:', cpf,'/senha: ', senha)
+
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
